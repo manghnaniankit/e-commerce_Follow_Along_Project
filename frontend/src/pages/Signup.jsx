4c68e5b5694f39615e51fb86503eee6c0f7ad549
manghnaniankit <ankit.manghnani@kalvium.community>
2025-01-21 16:29:38 +0530

    made changes in frontend login page

---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import Signup from '../components/auth/Signup';
+// function SignupPage() {
+//    return (
+//       <div>
+//        < Login/>
+//      </div>
+//     )
+// }
+// export default SignupPage;
