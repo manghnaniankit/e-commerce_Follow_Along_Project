37eed99a5a75c82195a3a18bafe60b340ee72a8a
manghnaniankit <ankit.manghnani@kalvium.community>
2025-02-25 11:01:35 +0530

    milestone 17

--- backend/model/user.js
+++ backend/model/user.js
@@ -16,10 +16,60 @@ const userSchema = new mongoose.Schema({
     minLength: [4, "Password should be greater than 4 characters"],
     select: false,
   },
-  avatar:{
+  phoneNumber:{
+    type: Number,
+  },
+  addresses:[
+    {
+      country: {
+        type: String,
+      },
+      city:{
+        type: String,
+      },
+      address1:{
+        type: String,
+      },
+      address2:{
+        type: String,
+      },
+      zipCode:{
+        type: Number,
+      },
+      addressType:{
+        type: String,
+      },
+    }
+  ],
+  role:{
     type: String,
-    required: true,
+    default: "user",
+  },
+  avatar:{
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
  },
+ cart: [
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [1, "Quantity cannot be less than 1"],
+      default: 1,
+    },
+  },
+],
  createdAt:{
   type: Date,
   default: Date.now(),
@@ -28,22 +78,22 @@ const userSchema = new mongoose.Schema({
  resetPasswordTime: Date,
 });
 //  Hash password
-// userSchema.pre("save", async function (next){
-//   if(!this.isModified("password")){
-//     next();
-//   }
-//   this.password = await bcrypt.hash(this.password, 10);
-// });
+userSchema.pre("save", async function (next){
+  if(!this.isModified("password")){
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
 // jwt token
-// userSchema.methods.getJwtToken = function () {
-//   return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
-//     expiresIn: process.env.JWT_EXPIRES,
-//   });
-// };
-// // compare password
-// userSchema.methods.comparePassword = async function (enteredPassword) {
-//   return await bcrypt.compare(enteredPassword, this.password);
-// };
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id}, process.env.JWT_SECRET_KEY,{
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 
 module.exports = mongoose.model("User", userSchema);
